df76a0f07c8aa14bbfa192c0b78fcacaa7ab20b5
RajDalvi08 <raj.233054101@vcet.edu.in>
2026-06-05 19:51:28 +0530

    Time: 4 ms (59.95%) | Memory: 65.1 MB (40.74%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -3,11 +3,12 @@ public:
     int findDuplicate(vector<int>& nums) {
         int slow = 0;
         int fast = 0;
-        while(true){
+        while(true){//bcos it is a cycle so no need to have condition of fast
             slow = nums[slow];
            fast = nums[nums[fast]];
         if(slow == fast){
             int ptr = 0;
+            //continue until it meets the requirement
             while(ptr != slow){
              ptr = nums[ptr];
              slow = nums[slow];
