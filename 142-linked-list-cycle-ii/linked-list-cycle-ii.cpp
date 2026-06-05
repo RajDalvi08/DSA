c7d064e76767896325f7f58f8bf3d131333a4411
RajDalvi08 <raj.233054101@vcet.edu.in>
2026-06-05 10:18:05 +0530

    Time: 3 ms (98.32%) | Memory: 11.4 MB (22.25%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
@@ -11,11 +11,14 @@ public:
     ListNode *detectCycle(ListNode *head) {
         ListNode* slow = head;
         ListNode* fast = head;
+        //CHECK FAST AND NEXT ELEMENT IS NOT NULL
     while(fast!=NULL && fast->next!=NULL){
         slow = slow->next;
         fast = fast->next->next;
+        //THEY WILL MEET AT SOME POINT
         if(slow == fast){
             ListNode* ptr = head;
+            //IF THE POINTER IS NOT EQUAL TO SLOW THAT MEANS ITS NOT THE FIRST ELEMENT
             while(ptr!=slow){
                 ptr = ptr->next;
                 slow = slow->next;
