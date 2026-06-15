fbee2669525beaa438eff6484e396f00ced5729d
RajDalvi08 <raj.233054101@vcet.edu.in>
2026-06-15 23:18:08 +0530

    Time: 0 ms (100.00%) | Memory: 22.7 MB (99.09%) - LeetSync

---
+++ 1028-interval-list-intersections/interval-list-intersections.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<int>> intervalIntersection(vector<vector<int>>& firstList,
+                                             vector<vector<int>>& secondList) {
+        vector<vector<int>> res;
+
+        int i = 0, j = 0;
+
+        while (i < firstList.size() && j < secondList.size()) {
+            int start = max(firstList[i][0], secondList[j][0]);
+            int end   = min(firstList[i][1], secondList[j][1]);
+
+            // Overlap exists
+            if (start <= end) {
+                res.push_back({start, end});
+            }
+
+            // Move the interval that finishes first
+            if (firstList[i][1] < secondList[j][1]) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+
+        return res;
+    }
+};
