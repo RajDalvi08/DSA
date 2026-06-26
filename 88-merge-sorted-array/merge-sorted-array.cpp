fae68ff39ed36822db01fc9f0e76aaa0489587d6
RajDalvi08 <raj.233054101@vcet.edu.in>
2026-06-26 19:16:21 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (72.35%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,36 +1,34 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i = 0;
-        int j = 0;
-        int res[m+n];
-        //compare as it is inside
-        while(i<m && j<n ){
-            if(nums1[i] <= nums2[j]){
-                res[i+j] = nums1[i];
-                
-                i++;
-            }
-            else{
-                 res[i+j] = nums2[j];
-                
-                j++;
-            }
-        }
-        //check as every individual arr over doent goes outside 
-        while(i<m){
-           res[i+j] = nums1[i];
-                
-                i++;
-        }
-        while(j<n){
-           res[i+j] = nums2[j];
-                
-                j++; 
-        }
-     for(int x=0; x<m+n; x++){
-        nums1[x]=res[x];
-          
-     }
+      int i=0;
+      int j=0;
+      vector<int>ans;
+
+      while(i<m && j<n){
+       if(nums1[i]<=nums2[j]){
+        ans.push_back(nums1[i]);
+        i++;
+       }
+       else{
+        ans.push_back(nums2[j]);
+        j++;
+       }
+      }
+
+       //Remaining elementss
+       while(i<m){
+          ans.push_back(nums1[i]);
+          i++;
+       }
+       while(j<n){
+         ans.push_back(nums2[j]);
+         j++;
+       }
+      
+      //copy the element to k
+      for(int k=0; k<m+n; k++){
+        nums1[k] = ans[k];
+      }
     }
 };
